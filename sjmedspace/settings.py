249b863451a464d41f9aae50e28da24b62e29f5b
vadlacharan <charanvadla27@gmail.com>
2025-03-11 00:24:40 +0530

    added whitenoise

--- sjmedspace/settings.py
+++ sjmedspace/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
@@ -57,7 +58,7 @@ MIDDLEWARE = [
 ]
 ROOT_URLCONF = "sjmedspace.urls"
 
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
